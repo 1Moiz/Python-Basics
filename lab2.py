76c5828a5e8c6a28c8a1a4880a76e84e6072f745
Moiz Ahmed <50263201+1Moiz@users.noreply.github.com>
2019-05-16 01:20:20 +0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,5 +1,5 @@
-#                                             #Assignment # 2
-#                                              #Chapter # 02
+                                             #Assignment # 2
+                                              #Chapter # 02
  #Q no 1
  username = 'Moiz Ahmed'
  print(username)
@@ -7,7 +7,7 @@
  message = 'Hello World'
  print(message)
  #Q no 3
- age = '23'
+ age = '21'
  certified =  'Certified in Python Language'
  print(username + "his age is " + age + " "+ certified)
  #Q no 4
@@ -23,114 +23,105 @@
  print("======================================================")
                                                #Chapter # 03
  #Q no 1
- age = '23'
-# print("I am " + age + " Years old")
-# #Q no 2
-# visit = '45'
-# print("You have visited this site "+visit+ " times" )
-# #Q no 3
-# birthyear = 1995
-# print("My birht year is ",birthyear)
-# print("Data type of my declared variable is number")
-# #Q no 4
-# visitor_name = 'Moiz Ahmed'
-# product_title = 'T-Shirts'
-# quantity = '5'
-# print(visitor_name+" ordered " + quantity+" " + product_title + " On Lucky One Mall")
-#
-#
-# print("======================================================")
-#                                                 #Chapter # 05
-# _1 = '_'
-# print("A variable name only contain in python is : "+ _1+" Called Hyphen")
-#
-#
-# print("======================================================")
-#                                                 # Chapter # 04
-# _1st_no = int(input("Enter 1st Number : "))
-# _2nd_no = int(input("Enter 2nd Number : "))
-# print("Sum of 1st and 2nd Number is : ",_1st_no + _2nd_no)
-# print("Sub of 1st and 2nd Number is : ",_1st_no - _2nd_no)
-# print("Mul of 1st and 2nd Number is : ",_1st_no * _2nd_no)
-# print("Div of 1st and 2nd Number is : ",_1st_no / _2nd_no)
-# # a = 7
-# # print("Initial value is : " ,a)
-# # a =+ 6
-# # print("Value after increment is : ",a)
-# ticket = 600
-# print("Total cost to buy 5 tickets to a movie is ",ticket*5," PKR" )
-# print("----------Print any table----------")
-# a = int(input("Enter number to print table : "))
-# for i in range(1,11):
-#     print(a,'x',i,'=',a*i)
-#
-#
-# print("======================================================")
-# print("----------Temperature Converter--------")
-# cel = float(input("Enter temperature in Celcius : "))
-# fah = (cel*9/5)+32
-# print("Celcius temperature in Fahrenheit",fah)
-# fah = int(input("Enter temperature in Fahrenheit  : "))
-# cel = (fah-32)*5/9
-# print("Fahrenheit temperature in Celcius",cel)
-# print("----------Shopping Cart----------")
-# p1 = int(input("Enter Price of 1st item : "))
-# q1 = int(input("Enter Quantity of 1st item is : "))
-# p2 = int(input("Enter Price of 2nd item : "))
-# q2 = int(input("Enter Quantity of 2nd item is : "))
-# shipping_charges = 100
-# print("Total cost of your item with Shipping charges is Rs 100  : ",(p1*q1)+(p2*q2)+100)
-#
-#
-# print("======================================================")
-# print("----------Calculating Percentage----------")
-# total_marks = 800
-# obtained_marks = 700
-# per = total_marks/obtained_marks*100
-# print("Total Marks : ",total_marks)
-# print("Marks Obtained : ",obtained_marks)
-# print("Percentage : ",per)
-# print("----------Currancy in PKR----------")
-# us = int(input("Enter US dollar : "))
-# sad = int(input("Enter Saudi Riyal : "))
-# print("Your US dollar in PKR is : ",us*141.62)
-# print("Your Saudi Riyal in PKR is : ",sad*37.76)
+ age = '21'
+ print("I am " + age + " Years old")
+ #Q no 2
+ visit = '45'
+ print("You have visited this site "+visit+ " times" )
+ #Q no 3
+ birthyear = 1997
+ print("My birth year is ",birthyear)
+ print("Data type of my declared variable is number")
+ #Q no 4
+ visitor_name = 'Moiz Ahmed'
+ product_title = 'T-Shirts'
+ quantity = '5'
+ print(visitor_name+" ordered " + quantity+" " + product_title + " On Lucky One Mall")
+
+
+ print("======================================================")
+                                                 #Chapter # 05
+ _1 = '_'
+ print("A variable name only contain in python is : "+ _1+" Called Hyphen")
+
+
+ print("======================================================")
+                                                 # Chapter # 04
+ _1st_no = int(input("Enter 1st Number : "))
+ _2nd_no = int(input("Enter 2nd Number : "))
+ print("Sum of 1st and 2nd Number is : ",_1st_no + _2nd_no)
+ print("Sub of 1st and 2nd Number is : ",_1st_no - _2nd_no)
+ print("Mul of 1st and 2nd Number is : ",_1st_no * _2nd_no)
+ print("Div of 1st and 2nd Number is : ",_1st_no / _2nd_no)
+ a = 7
+ print("Initial value is : " ,a)
+ a =+1
+ print("Value after increment is : ",a)
+ ticket = 600
+ print("Total cost to buy 5 tickets to a movie is ",ticket*5," PKR" )
+ print("----------Print any table----------")
+ a = int(input("Enter number to print table : "))
+ for i in range(1,11):
+     print(a,'x',i,'=',a*i)
+
+
+ print("======================================================")
+ print("----------Temperature Converter--------")
+ cel = float(input("Enter temperature in Celcius : "))
+ fah = (cel*9/5)+32
+ print("Celcius temperature in Fahrenheit",fah)
+ fah = int(input("Enter temperature in Fahrenheit  : "))
+ cel = (fah-32)*5/9
+ print("Fahrenheit temperature in Celcius",cel)
+ print("----------Shopping Cart----------")
+ p1 = int(input("Enter Price of 1st item : "))
+ q1 = int(input("Enter Quantity of 1st item is : "))
+ p2 = int(input("Enter Price of 2nd item : "))
+ q2 = int(input("Enter Quantity of 2nd item is : "))
+ shipping_charges = 100
+ print("Total cost of your item with Shipping charges is Rs 100  : ",(p1*q1)+(p2*q2)+100)
+
+
+ print("======================================================")
+ print("----------Calculating Percentage----------")
+ total_marks = 800
+ obtained_marks = 700
+ per = total_marks/obtained_marks*100
+ print("Total Marks : ",total_marks)
+ print("Marks Obtained : ",obtained_marks)
+ print("Percentage : ",per)
+ print("----------Currancy in PKR----------")
+ us = int(input("Enter US dollar : "))
+ sad = int(input("Enter Saudi Riyal : "))
+ print("Your US dollar in PKR is : ",us*141.62)
+ print("Your Saudi Riyal in PKR is : ",sad*37.76)
 
 
 print("======================================================")
 print("---------Age Calculater----------")
-# cy = 2019
-# by = 1995
-# print("Current year : ",cy)
-# print("Birth Year : ",by)
-# print("Your age is : ",(cy-by))
-cy = int(input("Enter your current year :"))
-by = int(input("Enter your Birth year :"))
-print("Your age is : ",(cy - by))
+ cy = int(input("Enter Current year : "))
+ by = int(input("Enter Birth year : "))
+ print("Your age is : ",(cy-by))
+ print("======================================================")
+ print("----------The Geometrizer----------")
+ radius = int(input("Enter Radius of circle : "))
+ print("Radius of circle is : ",radius)
+ print("The circumference is : ",2*3.14*radius)
+ print("The Area is : ",3.14*radius*radius)
+ print("======================================================")
+ print("----------The Lifetime Supply Calculator----------")
+ snake = 'Lays'
+ current_age = 15
+ maximum_age = 65
+ amount = 3
+ print("Favourite Snake : ",snake)
+ print("Current age : ",current_age)
+ print("Estimated age : ",maximum_age)
+ print("Amount of Snake per day : ",amount)
+ print("You will need ",(maximum_age-current_age)*amount ," Lays to last until the ripe old age of 65")
 
-# print("======================================================")
-# print("----------The Geometrizer----------")
-# radius = int(input("Enter Radius of circle : "))
-# print("Radius of circle is : ",radius)
-# print("The circumference is : ",2*3.14*radius)
-# print("The Area is : ",3.14*radius*radius)
-#
-#
-#
-# print("======================================================")
-# print("----------The Lifetime Supply Calculator----------")
-# snake = 'Lays'
-# current_age = 15
-# maximum_age = 65
-# amount = 3
-# print("Favourite Snake : ",snake)
-# print("Current age : ",current_age)
-# print("Estimated age : ",maximum_age)
-# print("Amount of Snake per day : ",amount)
-# print("You will need ",(maximum_age-current_age)*amount ," Lays to last until the ripe old age of 65")
-#
-#
-# print("======================================================")
+
+
 
 
 
